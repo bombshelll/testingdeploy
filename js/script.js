97357d7fb3b8c5b31a8c7a3c1df67f6afffbb645
sabrina <wansabrina.ws@gmail.com>
2023-12-19 10:08:59 +0700

    deploy

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-import { gsap } from "gsap";
+import { gsap } from "gsap/dist/gsap";
     
-import { CustomEase } from "gsap/CustomEase";
-import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
+import { CustomEase } from "gsap/dist/CustomEase";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/dist/EasePack";
     
-import { Flip } from "gsap/Flip";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Observer } from "gsap/Observer";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { Draggable } from "gsap/Draggable";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { EaselPlugin } from "gsap/EaselPlugin";
-import { PixiPlugin } from "gsap/PixiPlugin";
-import { TextPlugin } from "gsap/TextPlugin";
+import { Flip } from "gsap/dist/Flip";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { Observer } from "gsap/dist/Observer";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+import { Draggable } from "gsap/dist/Draggable";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import { EaselPlugin } from "gsap/dist/EaselPlugin";
+import { PixiPlugin } from "gsap/dist/PixiPlugin";
+import { TextPlugin } from "gsap/dist/TextPlugin";
+
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
+
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
 
 
 gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
